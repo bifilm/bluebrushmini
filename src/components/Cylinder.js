6d743403e242d6eb9eebf634aad75838c722ee45
mememan <98369477+bifilm@users.noreply.github.com>
2023-01-30 12:54:12 -0800

    Add controls and cylinder and box

--- src/components/Cylinder.js
+++ src/components/Cylinder.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
+import { Edges, PivotControls } from "@react-three/drei";
 
 export default function Cylinder(props) {
   // This reference will give us direct access to the mesh
@@ -9,12 +10,41 @@ export default function Cylinder(props) {
   return (
     <div style={{ height: "100%" }}>
       <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <mesh>
-          <cylinderGeometry args={[1, 1, 5]} />
-          <meshStandardMaterial color={"orange"} />
-        </mesh>
+        <PivotControls
+          rotation={[0, -Math.PI / 2, 0]}
+          anchor={[1, -1, -1]}
+          scale={75}
+          depthTest={false}
+          fixed
+          lineWidth={2}
+        >
+          <group>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <mesh>
+              <boxGeometry args={[2, 5, 0.06]} />
+              <meshStandardMaterial color={"grey"} />
+              <Edges
+                scale={1}
+                threshold={80} // Display edges only when the angle between two faces exceeds this value (default=15 degrees)
+                color="black"
+              />
+            </mesh>
+            <mesh>
+              <cylinderGeometry args={[1, 1, 5]} />
+              <meshStandardMaterial
+                color={"red"}
+                transparent={true}
+                opacity={0.7}
+              />
+              <Edges
+                scale={1}
+                threshold={80} // Display edges only when the angle between two faces exceeds this value (default=15 degrees)
+                color="black"
+              />
+            </mesh>
+          </group>
+        </PivotControls>
       </Canvas>
     </div>
   );
