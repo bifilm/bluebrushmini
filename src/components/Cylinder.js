e75e60d5339aa057c690e6d00d120687f87e21ed
mememan <98369477+bifilm@users.noreply.github.com>
2023-01-30 14:45:05 -0800

    Add canvas toggling

--- src/components/Cylinder.js
+++ src/components/Cylinder.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Edges, OrbitControls } from "@react-three/drei";
+import { ReactSketchCanvas } from "react-sketch-canvas";
 
 export default function Cylinder(props) {
   const [cylinderVisible, setCylinderVisible] = useState(true);
+  const [toggleCanvas, setToggleCanvas] = useState(4);
+  const drawingCanvas2 = useRef();
   // This reference will give us direct access to the mesh
   // Set up state for the hovered and active state
   // Subscribe this component to the render-loop, rotate the mesh every frame
@@ -13,35 +16,44 @@ export default function Cylinder(props) {
     <div style={{ height: "100%" }}>
       <button
         onClick={() => {
+          drawingCanvas2.current.clearCanvas();
+        }}
+      >
+        Clear Canvas
+      </button>
+      <button
+        onClick={() => {
+          setToggleCanvas(toggleCanvas * -1);
+          console.log(toggleCanvas);
+        }}
+      >
+        Toggle which canvas
+      </button>
+      <button
+        onClick={() => {
           setCylinderVisible(!cylinderVisible);
         }}
       >
         HIDE OR SHOW CYLINDER
       </button>
-      <Canvas>
-        <group>
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <mesh>
-            <boxGeometry args={[2, 5, 0.06]} />
-            <meshStandardMaterial
-              color={"grey"}
-              transparent={true}
-              opacity={0.5}
-            />
-            <Edges
-              scale={1}
-              threshold={80} // Display edges only when the angle between two faces exceeds this value (default=15 degrees)
-              color="black"
-            />
-          </mesh>
-          {cylinderVisible && (
+      <div style={{ height: "100%", position: "relative" }}>
+        <ReactSketchCanvas
+          style={{ position: "absolute", zIndex: toggleCanvas }}
+          ref={drawingCanvas2}
+          strokeWidth={1}
+          strokeColor="red"
+          canvasColor="transparent"
+        />
+        <Canvas>
+          <group>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
             <mesh>
-              <cylinderGeometry args={[1, 1, 5]} />
+              <boxGeometry args={[2, 5, 0.06]} />
               <meshStandardMaterial
-                color={"red"}
+                color={"grey"}
                 transparent={true}
-                opacity={0.7}
+                opacity={0.5}
               />
               <Edges
                 scale={1}
@@ -49,10 +61,25 @@ export default function Cylinder(props) {
                 color="black"
               />
             </mesh>
-          )}
-        </group>
-        <OrbitControls />
-      </Canvas>
+            {cylinderVisible && (
+              <mesh>
+                <cylinderGeometry args={[1, 1, 5]} />
+                <meshStandardMaterial
+                  color={"blue"}
+                  transparent={true}
+                  opacity={0.7}
+                />
+                <Edges
+                  scale={1}
+                  threshold={80} // Display edges only when the angle between two faces exceeds this value (default=15 degrees)
+                  color="black"
+                />
+              </mesh>
+            )}
+          </group>
+          <OrbitControls />
+        </Canvas>
+      </div>
     </div>
   );
 }
