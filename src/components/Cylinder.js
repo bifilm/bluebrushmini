4e28e0aec6d2c6c56f800ae6ef560a33755ccf3c
mememan <98369477+bifilm@users.noreply.github.com>
2023-01-30 13:12:25 -0800

    Added orbit controls

--- src/components/Cylinder.js
+++ src/components/Cylinder.js
@@ -1,35 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Edges, PivotControls } from "@react-three/drei";
+import { Edges, OrbitControls } from "@react-three/drei";
 
 export default function Cylinder(props) {
+  const [cylinderVisible, setCylinderVisible] = useState(true);
   // This reference will give us direct access to the mesh
   // Set up state for the hovered and active state
   // Subscribe this component to the render-loop, rotate the mesh every frame
   // Return view, these are regular three.js elements expressed in JSX
+
   return (
     <div style={{ height: "100%" }}>
+      <button
+        onClick={() => {
+          setCylinderVisible(!cylinderVisible);
+        }}
+      >
+        HIDE OR SHOW CYLINDER
+      </button>
       <Canvas>
-        <PivotControls
-          rotation={[0, -Math.PI / 2, 0]}
-          anchor={[1, -1, -1]}
-          scale={75}
-          depthTest={false}
-          fixed
-          lineWidth={2}
-        >
-          <group>
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
-            <mesh>
-              <boxGeometry args={[2, 5, 0.06]} />
-              <meshStandardMaterial color={"grey"} />
-              <Edges
-                scale={1}
-                threshold={80} // Display edges only when the angle between two faces exceeds this value (default=15 degrees)
-                color="black"
-              />
-            </mesh>
+        <group>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <mesh>
+            <boxGeometry args={[2, 5, 0.06]} />
+            <meshStandardMaterial color={"grey"} />
+            <Edges
+              scale={1}
+              threshold={80} // Display edges only when the angle between two faces exceeds this value (default=15 degrees)
+              color="black"
+            />
+          </mesh>
+          {cylinderVisible && (
             <mesh>
               <cylinderGeometry args={[1, 1, 5]} />
               <meshStandardMaterial
@@ -43,8 +45,9 @@ export default function Cylinder(props) {
                 color="black"
               />
             </mesh>
-          </group>
-        </PivotControls>
+          )}
+        </group>
+        <OrbitControls />
       </Canvas>
     </div>
   );
