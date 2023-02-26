126e0402b4671c3754004ed25e32caf4eba6a2d9
mememan <98369477+bifilm@users.noreply.github.com>
2023-02-25 21:32:26 -0800

    Change box visisiblity

--- src/components/Box.js
+++ src/components/Box.js
@@ -18,7 +18,7 @@ export default function Box({
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial
           transparent={true}
-          opacity={boxVis ? 0.4 : 0}
+          opacity={boxVis ? 0.45 : 0}
           color="red"
         />
         {boxVis && (
