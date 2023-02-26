6b3c795b3c52d730e18a514672b314ff0a2382c3
mememan <98369477+bifilm@users.noreply.github.com>
2023-02-25 19:13:48 -0800

    Add box edges

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Edges, OrbitControls, Grid, Line } from "@react-three/drei";
 export default function Box({
   initialBoxVisibility,
   positionArray,
@@ -15,11 +16,14 @@ export default function Box({
         }}
       >
         <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial
-          transparent={true}
-          opacity={boxVis ? 0 : 1.0}
-          color="red"
-        />
+        <meshStandardMaterial transparent={true} opacity={0} color="red" />
+        {boxVis && (
+          <Edges
+            scale={1}
+            threshold={80} // Display edges only when the angle between two faces exceeds this value (default=15 degrees)
+            color="black"
+          />
+        )}
       </mesh>
       <mesh position={[0.5, 0.5, 0.5]}>
         <sphereGeometry args={[0.09, 32, 32]} />
